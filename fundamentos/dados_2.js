9a3d012635d12ddbbefc416a0362dc8e012be5e9
EGAMER26 <evandrogomes542@gmail.com>
2022-08-25 14:56:42 -0300

    aprende sobre dados, sobre variaveis, e strings

--- fundamentos/dados_2.js
+++ fundamentos/dados_2.js
@@ -4,3 +4,14 @@ let precoComDesconto = preco * (1 - desconto)
 
 console.log(19.9 * 0.6)
 console.log(precoComDesconto)
+
+let nome = "caderno ";
+var categoria = "papelaria";
+console.log("Produto: " + nome 
++ ", Categoria: " + categoria 
++ ", Preço: " + preco
++ ", desconto: " + desconto);
+
+console.log (1 + 1)
+console.log("1" + " " + 1)
+console.log("1 " + 1)
---
+++ fundamentos/dados_4.js
@@ -0,0 +1,18 @@
+const a = 3
+let b = 5
+
+// .....
+
+console.log(b)
+
+// .....
+
+b = a + 10
+b = b + 10
+
+// .....
+
+// console.log(a)
+console.log(b)
+
+// é mais saldavel para o programa ter mais constantes do que variaveis.
