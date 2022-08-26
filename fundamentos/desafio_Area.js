3636f409972a7a6c53b4a90a2a649c5bd829b4c1
EGAMER26 <evandrogomes542@gmail.com>
2022-08-26 14:33:38 -0300

    desafios

---
+++ fundamentos/desafio_Area.js
@@ -0,0 +1,7 @@
+let raio = 10
+const PI = 3.14
+let areaCirculo = PI * raio * raio
+console.log('area do circulo é= ' + areaCirculo + ' M²')
+
+areaCirculo = Math.PI * raio * raio
+console.log('area do circulo é= ' + areaCirculo + ' M²')
---
+++ fundamentos/desafio_Troca.js
@@ -0,0 +1,20 @@
+// let a = 7
+// let b = 94
+
+// b = b - 87
+// a = a + 87
+// console.log(a)
+// console.log(b)
+
+console.log("----------------------------")
+
+
+let a = 10;
+let b = 30;
+
+let temp = a;
+a = b;
+b = temp;
+
+console.log(a);
+console.log(b);
