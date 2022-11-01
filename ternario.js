e53d273c79d01f6dd5a2c3ed161a20df687223ff
linux EGAMER <evandrogomes542@gmail.com>
2022-11-01 11:45:36 -0300

    for1

--- ternario.js
+++
@@ -1,8 +0,0 @@
-const resultado = nota => nota >=7 ? 'aprovado' : 'reprovado'
-
-// -----------------------PODE SER TAMBEM-----------------------
-const resultado = nota => {
-    nota >=7 ? 'aprovado' : 'reprovado'
-}
-console.log(resultado(8))
-console.log(resultado(5 ))
